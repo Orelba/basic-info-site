d1a6987edcac63e35bbfc753ed51ac47c5e8ec9c
Orelba <88292816+Orelba@users.noreply.github.com>
2023-12-23 04:17:45 +0200

    Project migrate to express, html files have been moved to the public folder

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-const http = require('node:http')
-const fs = require('node:fs')
+const express = require('express')
+const path = require('path')
 
-let page404
+const app = express()
+const port = 8080
 
-fs.readFile('./404.html', (err, data) => {
-  page404 = data
+app.use(express.static('public'))
+
+// Using express.static serves 'index.html' automatically for the root route ('/')
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/about.html'))
 })
 
-const server = http.createServer((req, res) => {
-  const path = req.url
-  const filename = path === '/' ? './index.html' : `.${path}.html`
-
-  fs.readFile(filename, (err, data) => {
-    if (err) {
-      res.writeHead(404, { 'Content-Type': 'text/html' })
-      return res.end(page404)
-    }
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.write(data)
-    return res.end()
-  })
+app.get('/contact-me', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/contact-me.html'))
 })
 
-server.listen(8080, () => {
-  console.log('Server running')
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, '/public/404.html'))
 })
